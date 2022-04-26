9dad6bb585c926e27a25c0fe3094d681e2705dc5
lazebird@163.com <>
2022-04-26 11:10:46 +0800

    fix: optimize menu style

--- src/layout/menu.vue
+++ src/layout/menu.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div v-for="(r, index) in data" :key="index">
-      <RouterLink :style="'margin-left: ' + 20 * depth + 'px'" :to="calcPath(prefix, r.path)">{{ calcLabel(r.path) }}</RouterLink>
+      <RouterLink :style="'margin-left: ' + 20 * depth + 'px; color: ' + calcColor(prefix, r.path)" :to="calcPath(prefix, r.path)">{{ calcLabel(r.path) }}</RouterLink>
       <Menu v-if="r.children" :data="r.children" :depth="depth + 1" :prefix="calcPath(prefix, r.path)" />
     </div>
   </div>
@@ -10,10 +10,11 @@
   export default { name: 'Menu' };
 </script>
 <script setup>
-  import { RouterLink } from 'vue-router';
+  import { RouterLink, useRoute } from 'vue-router';
 
   defineProps({ data: { type: Array, required: true }, depth: { type: Number, default: 0 }, prefix: { type: String, default: '' } });
 
   const calcPath = (prefix, path) => (path.charAt(0) === '/' ? path : `${prefix}/${path}`);
   const calcLabel = (path) => (path.lastIndexOf('/') >= 0 ? path.slice(path.lastIndexOf('/') + 1) : path);
+  const calcColor = (prefix, path) => (calcPath(prefix, path) === useRoute().path ? 'blue' : 'gray');
 </script>
